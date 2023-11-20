f863697199dc2bdb69e78ff9f7fb9b1e7a24cd60
Daniel Sullivan <ojojoj89@gmail.com>
2023-11-20 13:12:34 -0600

    wrote code for database and install js files

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.log('Putting content in the database');
 
+const db = await openDB('jate', 1);
+const tx = db.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+await store.put(content);
+return tx.complete;
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.log('Getting content from the database');
 
+const db = await openDB('jate', 1);
+const tx = db.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+return store.getAll();
+}
 initdb();
